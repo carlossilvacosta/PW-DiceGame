1d0667b5d45c69d23109d345d9e43083daa0dc70
Carlos <carlossergiosilvacosta@gmail.com>
2023-05-27 02:17:35 -0300

    Add LocalStorage

--- index.js
+++ index.js
@@ -25,14 +25,60 @@ let jogador2 = {
 let empates = 0;
 let rodadaAtual = 1;
 
+const salvarEstadoJogo = () => {
+    localStorage.setItem('jogador1', JSON.stringify(jogador1));
+    localStorage.setItem('jogador2', JSON.stringify(jogador2));
+    localStorage.setItem('empates', empates.toString());
+    localStorage.setItem('rodadaAtual', rodadaAtual.toString());
+    localStorage.setItem('btJogarDado1Disabled', btJogarDado1.disabled);
+    localStorage.setItem('btJogarDado2Disabled', btJogarDado2.disabled);
+    localStorage.setItem('btReiniciarDisabled', btReiniciar.disabled);
+};
+
+const carregarEstadoJogo = () => {
+    const jogador1Data = localStorage.getItem('jogador1');
+    const jogador2Data = localStorage.getItem('jogador2');
+    const empatesData = localStorage.getItem('empates');
+    const rodadaAtualData = localStorage.getItem('rodadaAtual');
+    const resultadoRodadaData = localStorage.getItem('resultadoRodada');
+    const resultadoPartidaData = localStorage.getItem('resultadoPartida');
+    const valorJogadas1Data = localStorage.getItem('valorJogadas1');
+    const valorJogadas2Data = localStorage.getItem('valorJogadas2');
+    const btJogarDado1Disabled = localStorage.getItem('btJogarDado1Disabled');
+    const btJogarDado2Disabled = localStorage.getItem('btJogarDado2Disabled');
+    const btReiniciarDisabled = localStorage.getItem('btReiniciarDisabled');
+
+    if (jogador1Data && jogador2Data && empatesData && rodadaAtualData && resultadoRodadaData
+        && resultadoPartidaData && valorJogadas1Data && valorJogadas2Data && btJogarDado1Disabled
+        && btJogarDado2Disabled && btReiniciarDisabled) {
+        jogador1 = JSON.parse(jogador1Data);
+        jogador2 = JSON.parse(jogador2Data);
+        empates = parseInt(empatesData);
+        rodadaAtual = parseInt(rodadaAtualData);
+        placarJogador1.textContent = jogador1.pontos;
+        placarJogador2.textContent = jogador2.pontos;
+        placarEmpate.textContent = empates;
+        numeroRodada.textContent = rodadaAtual;
+        resultadoRodada.textContent = resultadoRodadaData;
+        resultadoPartida.textContent = resultadoPartidaData;
+        valorJogadas1.innerHTML = valorJogadas1Data;
+        valorJogadas2.innerHTML = valorJogadas2Data;
+        btJogarDado1.disabled = btJogarDado1Disabled === 'true';
+        btJogarDado2.disabled = btJogarDado2Disabled === 'true';
+        btReiniciar.disabled = btReiniciarDisabled === 'true';
+    }
+};
+
 const jogadaJogador1 = () => {
     jogador1.jogada = Math.floor(Math.random() * 6) + 1;
     const valor = document.createTextNode(jogador1.jogada);
     const celulaTabela = document.createElement("td");
     celulaTabela.appendChild(valor);
     valorJogadas1.appendChild(celulaTabela);
+    localStorage.setItem('valorJogadas1', valorJogadas1.innerHTML);
     btJogarDado1.disabled = true;
     btJogarDado2.disabled = false;
+    salvarEstadoJogo();
 }
 
 const jogadaJogador2 = () => {
@@ -41,8 +87,10 @@ const jogadaJogador2 = () => {
     const celulaTabela = document.createElement("td");
     celulaTabela.appendChild(valor);
     valorJogadas2.appendChild(celulaTabela);
+    localStorage.setItem('valorJogadas2', valorJogadas2.innerHTML);
     btJogarDado2.disabled = true;
     btReiniciar.disabled = false;
+    salvarEstadoJogo();
     calcularVencedor();
 }
 
@@ -96,6 +144,9 @@ const calcularVencedor = () => {
         btReiniciar.disabled = false;
         //resultadoPartida.textContent = '-';
     }
+    localStorage.setItem('resultadoRodada', resultadoRodada.textContent);
+    localStorage.setItem('resultadoPartida', resultadoPartida.textContent);
+    salvarEstadoJogo();
 }
 
 const reiniciar = () => {
@@ -114,8 +165,10 @@ const reiniciar = () => {
     valorJogadas1.textContent = '';
     valorJogadas2.textContent = '';
     resultadoRodada.textContent = '';
+    salvarEstadoJogo();
 }
 
 btJogarDado1.onclick = jogadaJogador1;
 btJogarDado2.onclick = jogadaJogador2;
 btReiniciar.onclick = reiniciar;
+carregarEstadoJogo();
